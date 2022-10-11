270f33d3289ef207bedb09187c6ac2918179c997
iremtekinn <tekinnirem@gmail.com>
2022-10-11 11:24:33 +0300

    sayfalar olustutuldu

---
+++ lib/Restaurant.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Restaurant extends StatefulWidget {
+  const Restaurant({super.key});
+
+  @override
+  State<Restaurant> createState() => _RestaurantState();
+}
+
+class _RestaurantState extends State<Restaurant> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
