00f8c7fa95296d869ef0b494d56f823f1fffd384
iremtekinn <tekinnirem@gmail.com>
2022-10-12 10:13:47 +0300

    splashscreen olusturuldu

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:foodapp2/widgets/backgroundimage.dart';
+import 'package:foodapp2/widgets/createAccountText.dart';
+import 'package:foodapp2/widgets/loginbutton.dart';
+import 'package:foodapp2/widgets/logintext.dart';
+import 'package:foodapp2/widgets/textAccount.dart';
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
@@ -11,6 +17,17 @@ class Splashscreen extends StatefulWidget {
 class _SplashscreenState extends State<Splashscreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xff010E16),
+      body: Column(
+        children: [
+          BackgroundImage(),
+          LoginText(),
+          LoginButton(),
+          textAccount(),
+          createAccountText(),
+        ],
+      ),
+    );
   }
 }
